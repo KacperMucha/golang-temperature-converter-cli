f8eb8a7be0dfe7b6b635aedb06fad8fbe1b69e8c
Kacper Mucha <kacper.mucha@predicagroup.com>
2020-09-04 13:28:26 +0200

    Fix temperature convert task code

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-		_, err := fmt.Scanln(&originValue)
+		_, err = fmt.Scanln(&originValue)
 		if err != nil {
 			printError(errReadingInput)
 		}
